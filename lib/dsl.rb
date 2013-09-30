b23c3a7afb40e94a6fa556681a8df1cad38ddc27
James Courtois <jcourtois@thoughtworks.com>
2013-09-30 12:13:16 -0500

    Undoing DSL refactoring that broke ActiveDoc.

--- lib/dsl.rb
+++ lib/dsl.rb
@@ -3,23 +3,19 @@ module DSL
   @page
 
   def where_we_have(*module_names)
+    puts module_names
     on_page_with(*module_names){|page| @page=page; yield page}
   end
-  #consider not using metaprogramming here
 
   define_method :step do |step_number, action_name, optional_args={}|
-    perform_step_for example, step_number, action_name, optional_args
-  end
-
-  #is there a gizmo dependency??
-  def perform_step_for example, step_number, action_name, arguments
     example.metadata[:doc_steps].store step_number, action_name
     args = optional_args[:with] || {}
 
-    if optional_args[:img]
+    if optional_args.has_key?(:img)
       example.metadata[:doc_pics].store step_number, optional_args[:img]
       args[:img] = optional_args[:img]
     end
     @page.perform action_name, args
   end
+
 end
